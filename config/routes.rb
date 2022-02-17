6b4b5ac9bbb4964443a8d20435f39ec0c1d1f946
km121500 <km121500@gmail.com>
2022-02-17 09:28:02 +0000

    [add]todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'lists#index'
 resources :genres
 resources :lists
+get "genre_search/:id", to: "lists#genre_search",as: 'genre_search'
 end
